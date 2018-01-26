91d8ced2e40c5257a5eda23bf8546e91257fecd5
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2018-01-26 15:18:19 +0100

    set local forward hostname resolution

--- deployment/install/deployment.sh
+++ deployment/install/deployment.sh
@@ -3,19 +3,6 @@
 set -e
 set -x
 
-_ss_set_hostname() {
-  echo "`ss-get hostname`    $(hostname)" >> /etc/hosts
-  ss-set machine-hn `hostname`
-}
-
-_get_cloud_hostname() {
-  server_ip=`ss-get hostname`
-  server_hostname=`ss-get machine-hn`
-}
-
-_ss_set_hostname
-_get_cloud_hostname
-
 ss_username=`ss-get ss-username`
 ss_password=`ss-get ss-password`
 
@@ -26,6 +13,4 @@ pip install -r ~/SAR-framework/deployment/dmm/requirements.txt
 cd ~/SAR-framework/deployment/dmm/
 python server_dmm.py \
     $ss_username \
-    $ss_password \
-    $server_ip \
-    $server_hostname &
+    $ss_password &
--- deployment/install/pre_install.sh
+++ deployment/install/pre_install.sh
@@ -3,6 +3,16 @@
 set -e
 set -x
 
+# Returns first global IPv4 address.
+_get_ip() {
+    ip addr | awk '/inet .*global/ { split($2, x, "/"); print x[1] }' | head -1
+}
+
+set_local_forward_resolution() {
+    ip=$(_get_ip)
+    echo "$ip $(hostname)" >> /etc/hosts
+}
+
 elk_repo() {
     apt-get update
     wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
@@ -14,5 +24,6 @@ ntp_conf() {
   timedatectl set-ntp no
 }
 
+set_local_forward_resolution
 elk_repo
 ntp_conf
