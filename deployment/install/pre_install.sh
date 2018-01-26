0e2cdfd8ddb4ae7675d3776bf75166b42586e300
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2018-01-26 13:40:55 +0100

    don't need to install apt-trasport-https; fix storing apt .list for ES.

--- deployment/install/pre_install.sh
+++ deployment/install/pre_install.sh
@@ -5,9 +5,8 @@ set -x
 
 elk_repo() {
     apt-get update
-    apt-get install -y apt-transport-https
     wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-    sudo echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" \
+    sudo echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" > \
         /etc/apt/sources.list.d/elastic-5.x.list
 }
 
