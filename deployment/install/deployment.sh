7d36dc3c57fbda0597b090ebe54716b54390ccf4
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2018-01-26 15:22:14 +0100

    fixed usage of path to SAR framework

--- deployment/install/deployment.sh
+++ deployment/install/deployment.sh
@@ -6,11 +6,13 @@ set -x
 ss_username=`ss-get ss-username`
 ss_password=`ss-get ss-password`
 
-git clone https://github.com/SixSq/SAR-framework.git
+SAR_PATH=~/SAR-framework
 
-pip install -r ~/SAR-framework/deployment/dmm/requirements.txt
+git clone https://github.com/SixSq/SAR-framework.git $SAR_PATH
 
-cd ~/SAR-framework/deployment/dmm/
+pip install -r $SAR_PATH/deployment/dmm/requirements.txt
+
+cd $SAR_PATH/deployment/dmm/
 python server_dmm.py \
     $ss_username \
     $ss_password &
